8e8faa692204544c452acee76fbf689a87d04219
Asad 629 <asad.ismail@nkutechnologies.com>
2024-10-30 15:30:40 +0500

    push project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Click me</Button>
+      <button className="font-bold"> tailwind</button>
+    </>
+  );
+}
+
+export default App;
